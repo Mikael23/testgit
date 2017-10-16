a0684a07ab437dc5aff3ff08b3585fb346b019b5
mikhailpovolotskiy <mpj2310@gmail.com>
2017-10-16 22:40:52 +0300

    DEVXOL-15477 implemented method for printing

--- src/main/java/com/testgit/Main.java
+++ src/main/java/com/testgit/Main.java
@@ -3,6 +3,10 @@ package com.testgit;
 public class Main {
 
     public static void main(String[] args) {
+        printHello();
+    }
+
+    static void printHello() {
         System.out.println("Hello git!");
     }
 
